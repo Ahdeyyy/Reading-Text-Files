1e90197715527ac2507e490cc89d8a8b95a13dfc
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-05-26 16:26:19 +0100

    fixes
    -special characters no longer included in words

--- main.py
+++ main.py
@@ -3,17 +3,30 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+def remove_symbols(text):
+    symbols = ['.',"/",'!','?',',',':',';','-','_']
+    new_text = ""
+    for word in text :
+        if word in symbols:
+            continue
+        else :
+            new_text += word
+
+    return new_text
 def read_file_content(filename):
     # [assignment] Add your code here 
     file = open(filename,"r+")
-    return file.read()
+    text = file.read()
+    file.close()
+    return text
 
 
-def count_words():
-    text = read_file_content("./story.txt")
+def count_words(text):
+    # [assignment] Add your code here
+    #text = read_file_content("./story.txt")
+    text = remove_symbols(text)
     text = text.lower()
     words = text.split()
-    # [assignment] Add your code here
     dict = {}
     for word in words:
         if dict.get(word) == None :
@@ -22,5 +35,5 @@ def count_words():
             dict[word] = dict.get(word) + 1
     return dict
 
-print(read_file_content("./story.txt"))
-print(count_words())
+#print(read_file_content("./story.txt"))
+print(count_words("The cake is done. It is a big cake!"))
